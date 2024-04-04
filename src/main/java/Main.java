4f3bbcd0712e4070ef8e505585a140fb62d3a7f1
AntSavit <anton6551@mail.ru>
2024-04-04 20:44:14 +0500

    Revert "Main prep for merge"
    This reverts commit a6cdcdd298486a78c6c8e392f609f7af55e8a5d8.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,10 @@
 import controller.ApplicationController;
 import view.MainApplicationFrame;
 import view.visualizers.GameVisualizer;
+
 import java.awt.Frame;
+
 import javax.swing.SwingUtilities;
-import java.util.Locale;
-import java.util.ResourceBundle;
-import javax.swing.*;
 
 /**
  * Main класс
@@ -13,26 +12,18 @@ import javax.swing.*;
  */
 public class Main
 {
-    public static void main(String[] args)
-    {
-        Locale locale = new Locale("ru", "RU");
+    public static void main(String[] args) {
+
         GameVisualizer m_visualizer = new GameVisualizer();
         ApplicationController m_controller = new ApplicationController(m_visualizer);
 
         try
         {
-            UIManager.put("OptionPane.yesButtonText",
-                    ResourceBundle.getBundle("lang", locale)
-                            .getString("option.yes"));
-            UIManager.put("OptionPane.noButtonText",
-                    ResourceBundle.getBundle("lang", locale)
-                            .getString("option.no"));
             //UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
             //UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
             //UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
             //UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
         }
-
         catch (Exception e)
         {
             e.printStackTrace();
